c918701bd7451e9c26c8afe8bad20f35aeb124ff
ashwani46 <yadavashwani4651@gmail.com>
2026-04-08 18:24:36 +0530

    update 01_basic/02_DataTypes.js

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Enabling strict mode for better error handling and to prevent certain actions that are considered bad practice in JavaScript newer versions. It helps catch common coding mistakes and "unsafe" actions such as assigning values to undeclared variables.
+
+// Data Types in JavaScript 
+let name = "Alice"; // String
+let age = 30; // Number
+let isStudent = false; // Boolean
+let hobbies = ["reading", "traveling", "coding"]; // Array
+let address = {    
+    street: "123 Main St",
+    city: "New York",
+    zipCode: "10001"
+}; // Object
+let nullValue = null; // Null
+let undefinedValue; // Undefined
+console.log("Name:", name); // Logging the value of the variable 'name' to the console cut signifying that it is a string data type 
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Hobbies:", hobbies);
+console.log("Address:", address);
+console.log("Null Value:", nullValue);
+console.log("Undefined Value:", undefinedValue);
