dc0f21e94a470f39e3c827ae89c1c6f4046c43a4
ashwani46 <yadavashwani4651@gmail.com>
2026-04-09 03:18:42 +0530

    updated

--- 01_basic/02_DataTypes.js
+++ 01_basic/02_DataTypes.js
@@ -1,9 +1,14 @@
 "use strict"; // Enabling strict mode for better error handling and to prevent certain actions that are considered bad practice in JavaScript newer versions. It helps catch common coding mistakes and "unsafe" actions such as assigning values to undeclared variables.
-
+//alert(3 + 4); // Displaying an alert box with the result of the expression 3 + 4, which will show "7" in the alert dialog.
 // Data Types in JavaScript 
 let name = "Alice"; // String
 let age = 30; // Number
 let isStudent = false; // Boolean
+//symbol and bigint are also data types in JavaScript, but they are less commonly used compared to the ones mentioned above. Symbol is a unique and immutable primitive value that can be used as an identifier for object properties, while BigInt is a numeric data type that can represent integers with arbitrary precision, allowing for very large numbers beyond the limits of the Number type.
+let symbolExample = Symbol("uniqueIdentifier"); // Symbol
+let bigIntExample = 1234567890123456789012345678901234567890n; // BigInt, the 'n' at the end indicates that this is a BigInt literal 
+
+
 let hobbies = ["reading", "traveling", "coding"]; // Array
 let address = {    
     street: "123 Main St",
@@ -13,9 +18,13 @@ let address = {
 let nullValue = null; // Null
 let undefinedValue; // Undefined
 console.log("Name:", name); // Logging the value of the variable 'name' to the console cut signifying that it is a string data type 
+
 console.log("Age:", age);
 console.log("Is Student:", isStudent);
 console.log("Hobbies:", hobbies);
 console.log("Address:", address);
 console.log("Null Value:", nullValue);
 console.log("Undefined Value:", undefinedValue);
+console.log("Symbol Example:", symbolExample);
+console.log("BigInt Example:", bigIntExample);
+console.log("Data Types:", typeof name, typeof age, typeof isStudent, typeof hobbies, typeof address, typeof nullValue, typeof undefinedValue, typeof symbolExample, typeof bigIntExample); // Logging the data types of the variables to the console using the 'typeof' operator 
---
+++ 01_basic/03_Conversionoperator.js
@@ -0,0 +1,28 @@
+let score ="100"; // A string variable representing a score
+let numericScore = Number(score); // Converting the string to a number using the Number() function
+console.log("Numeric Score:", numericScore); // Logging the numeric score to the console    
+console.log("Type of Numeric Score:", typeof numericScore); // Logging the data type of numericScore to the console
+let score1="abc100"; // A string that cannot be converted to a number
+let invalidScore = Number(score1); // This will result in NaN (Not a Number)
+console.log("Invalid Score:", invalidScore); // Logging the invalid score to the console
+console.log("Type of Invalid Score:", typeof invalidScore); // Logging the data type of invalidScore to the console
+let score2=true; // A boolean variable
+let numericScore2 = Number(score2); // Converting the boolean to a number (true will be converted to 1)
+console.log("Numeric Score from Boolean:", numericScore2); // Logging the numeric score obtained from the boolean conversion to the console
+console.log("Type of Numeric Score from Boolean:", typeof numericScore2); // Logging the data type of numericScore2 to the console  
+let score3="ashwani"; // A string that cannot be converted to a number
+let invalidScore2 = Number(score3); // This will also result in NaN (Not a Number)
+console.log("Invalid Score 2:", invalidScore2); // Logging the invalid score to the console
+console.log("Type of Invalid Score 2:", typeof invalidScore2); // Logging the data type of invalidScore2 to the console
+let score4= null; // A null value
+let numericScore4 = Number(score4); // Converting null to a number (null will be converted to 0)
+console.log("Numeric Score from Null:", numericScore4); // Logging the numeric score obtained from the null conversion to the console
+console.log("Type of Numeric Score from Null:", typeof numericScore4); // Logging the data type of numericScore4 to the console
+let score5=undefined; // An undefined value
+let numericScore5 = Number(score5); // Converting undefined to a number (undefined will be converted to NaN)
+console.log("Numeric Score from Undefined:", numericScore5); // Logging the numeric score obtained from the undefined conversion to the console
+console.log("Type of Numeric Score from Undefined:", typeof numericScore5); // Logging the data type of numericScore5 to the console
+let someno=100;
+let stringScore = String(someno); // Converting the number to a string using the String() function
+console.log("String Score:", stringScore);
+console.log("Type of String Score:", typeof stringScore); // Logging the data type of stringScore to the console
